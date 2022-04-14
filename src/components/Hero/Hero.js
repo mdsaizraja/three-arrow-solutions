6edd65e6da25f48e6c01b18027c77764486fe543
SpydyWeb <ankitkumar65438@gmail.com>
2022-04-14 18:40:53 +0530

    6:50

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import Lottie from "react-lottie-player";
 import lottieJson from "../../animation/38834-service-animation.json";
 import {
@@ -12,39 +12,49 @@ import {
   MainSection,
 } from "./HeroStyles";
 
-const Hero = () => (
-  <MainSection>
-    <LeftSection>
-      <HeadingSection>
-        Let's Build Something amazing with three arrow thechnologies
-      </HeadingSection>
-      <SubHeadingSection>
-        We design a road map towards the success of your Business in this
-        Digital World.
-      </SubHeadingSection>
-      <MaininputBox>
-        <InputBox
-          className="p-8"
-          type="text"
-          placeholder="Your Email Address"
-        ></InputBox>
-        <ButtonBox>Get Started</ButtonBox>
-      </MaininputBox>
-    </LeftSection>
-    <RightSection>
-      {/*<Lottie*/}
-      {/*  src="https://assets5.lottiefiles.com/packages/lf20_y3qfynfr.json"*/}
-      {/*  background="transparent"*/}
-      {/*  speed="1"*/}
-      {/*  autoplay*/}
-      {/*/>*/}
-      <Lottie
-        animationData={lottieJson}
-        play
-        style={{ width: "457px", height: "457px" }}
-      />
-    </RightSection>
-  </MainSection>
-);
+const Hero = () => {
+  const ref = useRef(null);
+  const startLottieAnim = (anim) => {
+    this.lottieAnim = anim;
+    if (anim) {
+      this.lottieAnim.play();
+    }
+  };
+  return (
+    <MainSection>
+      <LeftSection>
+        <HeadingSection>
+          Let's Build Something amazing with three arrow thechnologies
+        </HeadingSection>
+        <SubHeadingSection>
+          We design a road map towards the success of your Business in this
+          Digital World.
+        </SubHeadingSection>
+        <MaininputBox>
+          <InputBox
+            className="p-8"
+            type="text"
+            placeholder="Your Email Address"
+          ></InputBox>
+          <ButtonBox>Get Started</ButtonBox>
+        </MaininputBox>
+      </LeftSection>
+      <RightSection>
+        {/*<Lottie*/}
+        {/*  src="https://assets5.lottiefiles.com/packages/lf20_y3qfynfr.json"*/}
+        {/*  background="transparent"*/}
+        {/*  speed="1"*/}
+        {/*  autoplay*/}
+        {/*/>*/}
+        <Lottie
+          ref={(c) => startLottieAnim(c)}
+          animationData={lottieJson}
+          play
+          style={{ width: "457px", height: "457px" }}
+        />
+      </RightSection>
+    </MainSection>
+  );
+};
 
 export default Hero;
