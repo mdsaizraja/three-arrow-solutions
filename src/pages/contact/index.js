843e07242e2673e8fd57081aa293de47b0db3cae
Nishant <na88277@gmail.com>
2022-05-11 11:11:55 +0530

    - solved build issue.

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,7 +1,11 @@
 import React from "react";
+import styled from "styled-components";
 import Header from "../../components/Header/Header";
 import Contact from "../../components/Contact/Contact";
-import {Wrap} from "./style"
+
+export const Wrap = styled.div`
+  background: linear-gradient(89.97deg, var(--gradient-one) 1.84%, var(--gradient-two) 102.67%);
+`;
 
 const index = () => {
     return (
--- src/pages/contact/style.js
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-export const Wrap = styled.div`
-  background: linear-gradient(89.97deg, var(--gradient-one) 1.84%, var(--gradient-two) 102.67%);
-`;
